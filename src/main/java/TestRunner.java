9ee6ebc317cf0b9bd34126479802bc9895f536cc
Victoria Chetrusca <124374613+ChetruscaV@users.noreply.github.com>
2023-04-24 14:19:25 +0300

    Creating a new driver and opening a website

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,5 +1,13 @@
+import Managers.WebDriverManager;
+
 public class TestRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  InterruptedException{
+        WebDriverManager webDriverManager=new WebDriverManager("CHROME");
+        webDriverManager.getDriver().get("https://testautomationu.applitools.com/");
+
+        Thread.sleep(15000);
+        webDriverManager.closeDriver();
+
 
     }
 }
