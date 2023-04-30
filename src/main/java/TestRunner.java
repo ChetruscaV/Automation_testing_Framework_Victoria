362012bf29dde1413c65cb658b2b03d5437715a7
Victoria Chetrusca <124374613+ChetruscaV@users.noreply.github.com>
2023-04-30 20:47:11 +0300

    Added pageObject classes

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,9 +1,17 @@
 import Managers.WebDriverManager;
+import pageObjects.HomePage;
+import pageObjects.RegisterPage;
 
 public class TestRunner {
     public static void main(String[] args) throws  InterruptedException{
         WebDriverManager webDriverManager=new WebDriverManager("CHROME");
-        webDriverManager.getDriver().get("https://testautomationu.applitools.com/");
+        webDriverManager.getDriver().get("https://demo.opencart.com/");
+
+        HomePage homePage=new HomePage(webDriverManager.getDriver());
+        homePage.navigateToRegisterPage();
+
+        RegisterPage registerPage=new RegisterPage(webDriverManager.getDriver());
+        registerPage.fillInTheRegisterForm("Chetrusca","Victoria","chetruscav@mail.ru","123654789");
 
         Thread.sleep(15000);
         webDriverManager.closeDriver();
